d901e54bfc8b0b0bf5c9fa309432ad5fbc363a53
insafmechergui <insaf_mechergui@outlook.com>
2021-09-06 12:12:25 +0100

    organizing widgets

--- lib/answer.dart
+++ lib/answer.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
 
-  final Function() selectHandler;
+  final VoidCallback selectHandler;
   final String answerText;
 
   Answer(this.selectHandler, this.answerText);
@@ -10,13 +10,13 @@ class Answer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    width: double.infinity,
-    child: RaisedButton(
-    color:Colors.blue,
-    textColor: Colors.white,
-    child: Text(answerText),
-    onPressed: selectHandler,
-  ),
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
-import 'answer.dart';
-void main(){
+import 'quiz.dart';
+import 'result.dart';
+
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -13,52 +14,81 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
+  final _questions = const[
+  {
+  'questionText': 'whats your favorite color?',
+  'answers':[
+  {'text': 'Black', 'score': 10},
+  {'text':'Red', 'score':5},
+  {'text':'Green', 'score':3},
+  {'text':'White','score': 0},
+],
+},
+{
+'questionText': 'whats your favorite animal?',
+'answers':[
+  {'text':'Rabbit','score':3},
+  {'text':'Snake','score':11},
+  {'text':'Elephant','score':5},
+  {'text':'Lion','score':9},
+],
+},
+{
+'questionText': 'who\'s your favorite instructor?',
+'answers':[
+  {'text':'Matt','score':1},
+  {'text':'Max', 'score':2},
+  {'text':'Alex','score':3},
+  {'text':'Lean','score':4},
+],
+},
+{
+'questionText': 'What\'s the meaning of Afflatus?',
+'answers': [
+  {'text':'ghost','score':11},
+  {'text':'inspiration','score':3},
+  {'text':'lifeless','score':8},
+  {'text':'greedy','score':10}
+  ],
+},
+];
 
-  void _answerQuestion(){
-    setState(() {
-      _questionIndex = _questionIndex + 1;
+var _questionIndex = 0;
+var _totalScore = 0;
 
-    });
-    print(_questionIndex);
+void _answerQuestion(int score) {
+
+  _totalScore += score;
+
+  setState(() {
+    _questionIndex = _questionIndex + 1;
+  });
+
+  print(_questionIndex);
+
+  if (_questionIndex < _questions.length) {
+    print('We have more question');
   }
+  else {
+    print('No more questions!');
+  }
+}
 
-  @override
-  Widget build(BuildContext context) {
-    var questions=[
-      {
-        'questionText': 'whats your favorite color?',
-        'answers':['Black','Red','Green','White'],
-      },
-      {
-        'questionText': 'whats your favorite animal?',
-        'answers':['Rabbit','Snake','Elephant','Lion'],
-      },
-      {
-        'questionText': 'who\'s your favorite instructor?',
-        'answers':['Matt','Max','Alex','Lean'],
-      },
-    {
-        'questionText': 'What\'s the meaning of Afflatus?',
-        'answers': ['ghost','inspiration','lifeless','greedy'],
-    },
-    ];
-    return MaterialApp(
-      home: Scaffold(
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome'),
         ),
-        body:Column(
-          children:[
-            Question(
-              questions[_questionIndex]['questionText']?.toString() ?? '',
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>).map((answer){
-             return Answer(_answerQuestion, answer);
-           }) .toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+          answerQuestion: _answerQuestion,
+          questionIndex: _questionIndex,
+          questions: _questions,
+        )
+            : Result(_totalScore)
+
     ),
-    );
-  }
-}
+  );
+}}
